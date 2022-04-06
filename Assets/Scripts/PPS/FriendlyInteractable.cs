6e8bce14d3ef9fd36aa8f5b2aa2c21a482e4ec1c
Peter Leth <16867891+kuff@users.noreply.github.com>
2022-04-06 18:28:20 +0200

    Add fix for branching

--- Assets/Scripts/PPS/FriendlyInteractable.cs
+++ Assets/Scripts/PPS/FriendlyInteractable.cs
@@ -45,35 +45,35 @@ public class FriendlyInteractable : Interactable
             if (this.parentTask.IsActive())
             {
                 this.ChangeMaterial(this.highlightMaterial);
-                //if (!this.gameObject.GetComponent<Task>().isGlovingImmidiately) return;
                 base.OnHandHoverBegin(new Hand());  // fake hand don't sue me Valve
             }
         }
         StartCoroutine(DelayedCallback());
     }
 
+    public void UnsuccessfulResolve()
+    {
+        this.OnDetachedFromHand(new Hand());
+    }
+
     public void DebugEnterHover()
     {
-        //Debug.Log("DebugEnterHover");
         this.OnHandHoverBegin(new Hand());
     }
 
     public void DebugExitHover()
     {
-        //Debug.Log("DebugExitHover");
         this.OnHandHoverEnd(new Hand());
     }
 
     public void DebugGrab()
     {
-        //Debug.Log("DebugGrab");
         if (this.parentTask.IsActive())
             this.parentTask.Grab(new Hand(), true);
     }
 
     public void DebugDrop()
     {
-        //Debug.Log("DebugDrop");
         this.parentTask.Drop(new Hand(), true);
     }
 
@@ -86,14 +86,11 @@ public class FriendlyInteractable : Interactable
     protected override void OnHandHoverBegin(Hand hand)
     {
         if (!this.parentTask.IsActive()) return;
-        if (hand != null) hand.ShowGrabHint();
-
-        //Debug.Log(this.gameObject.name + "->OnHandHoverBegin");
+        hand?.ShowGrabHint();
 
         this.isActuallyHovering = true;
         this.parentTask.EnterHover(hand);
 
-        //if (!this.parentTask.isGlovingImmidiately) return;
         base.OnHandHoverEnd(hand);
         this.ChangeMaterial(this.hoverMaterial);
         base.OnHandHoverBegin(hand);
@@ -103,12 +100,9 @@ public class FriendlyInteractable : Interactable
     {
         if (!this.parentTask.IsActive()) return;
 
-        //Debug.Log(this.gameObject.name + "->OnHandHoverEnd");
-
         this.isActuallyHovering = false;
         this.parentTask.ExitHover(hand);
 
-        //if (!this.parentTask.isGlovingImmidiately) return;
         base.OnHandHoverEnd(hand);
         this.ChangeMaterial(this.highlightMaterial);
         base.OnHandHoverBegin(hand);
@@ -118,20 +112,15 @@ public class FriendlyInteractable : Interactable
     {
         if (!this.parentTask.IsActive()) return;
 
-        //Debug.Log(this.gameObject.name + "->OnAttachedToHand");
-
         this.parentTask.Grab(hand);
         this.ChangeMaterial(this.grabMaterial);
 
         if (this.parentTask.IsMovable()) base.OnAttachedToHand(hand);
-        //this.parentTask.Resolve();
     }
 
     protected override void OnDetachedFromHand(Hand hand)
     {
         if (!this.parentTask.IsActive()) return;
-
-        //Debug.Log(this.gameObject.name + "->OnDetachedFromHand");
 
         this.parentTask.Drop(hand);
         this.ChangeMaterial(this.highlightMaterial);
